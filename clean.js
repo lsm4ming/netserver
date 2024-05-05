53023519e05dd19bc84ba37abcde44bf166893a9
lsm4ming <lsm19980104@gmail.com>
2024-05-06 02:05:40 +0800

    1

--- clean.js
+++ clean.js
@@ -4,7 +4,7 @@ const path = require('path');
 let delCount = 0;
 
 const ignoreFiles = new Set(['.git', '.vscode', '.gitignore']);
-const removeFileRule = ['cmake_install.cmake', 'CMakeCache.txt', 'CMakeFiles' , 'Makefile'];
+const removeFileRule = ['cmake_install.cmake', 'CMakeCache.txt', 'CMakeFiles', 'Makefile'];
 
 const clean = (dirPath) => {
     // 读取目录中的所有文件和子目录
