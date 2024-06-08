ed05f187ed2cd6524d5f1021e606114b32f7722f
lsm <lsm19980104@gmail.com>
2024-06-08 17:10:40 +0800

    response

--- httpclient/httpclient.cpp
+++ httpclient/httpclient.cpp
@@ -13,6 +13,13 @@ HttpClient *HttpClient::addQuery(const String &key, const String &value)
     return this;
 }
 
+HttpClient *HttpClient::setBody(char *body, size_t bodyLength)
+{
+    this->body = body;
+    this->bodyLength = bodyLength;
+    return this;
+}
+
 HttpClient *HttpClient::post(const String &url)
 {
     return this->doSend("POST", url);
@@ -37,6 +44,11 @@ HttpClient *HttpClient::doSend(const String &method, const String &url)
         this->path = url_match_result[3].str();
         this->queryRaw = url_match_result[4].str();
 
+        if (this->path.empty())
+        {
+            this->path = "/";
+        }
+
         this->query = this->parseQuery(this->queryRaw);
         this->send();
     }
@@ -47,17 +59,18 @@ HttpClient *HttpClient::doSend(const String &method, const String &url)
     return this;
 }
 
-String HttpClient::host_to_ip(String hostname)
+String HttpClient::host_to_ip(const String &hostname)
 {
-    auto *host_entry = gethostbyname(hostname.c_str()); // dns
-    if (host_entry)
+    auto *he = gethostbyname(hostname.c_str()); // dns
+    if (he == nullptr)
     {
-        return inet_ntoa(*(struct in_addr *)*host_entry->h_addr_list);
+        return "";
     }
-    return "";
+    auto **addr_list = (struct in_addr **)he->h_addr_list;
+    return inet_ntoa(*addr_list[0]);
 }
 
-int HttpClient::http_create_socket(String ip)
+int HttpClient::http_create_socket(const String &ip)
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in sin = {0};
@@ -68,7 +81,7 @@ int HttpClient::http_create_socket(String ip)
     {
         return -1;
     }
-    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+    // fcntl(sockfd, F_SETFL, O_NONBLOCK);
     return sockfd;
 }
 
@@ -104,11 +117,109 @@ Header HttpClient::parseQuery(const String &queryRaw)
     return queryMap;
 }
 
+String HttpClient::joinValues(const Vector<String> &values)
+{
+    OsStringStream result;
+    for (auto it = values.begin(); it != values.end(); ++it)
+    {
+        if (it != values.begin())
+        {
+            result << ", ";
+        }
+        result << *it;
+    }
+    return result.str();
+}
+
 void HttpClient::send()
 {
     // 组装请求url
     this->rawUrl = assembleUrl();
     std::cout << "请求url=" << this->rawUrl << std ::endl;
+
+    // 将domain转为IP地址
+    String ip = this->host_to_ip(this->domain);
+
+    // 建立TCP连接
+    int socketFd = this->http_create_socket(ip);
+    // set_socket_timeout(socketFd, 10);
+
+    OsStringStream stream;
+
+    // 写请求行
+    stream << method << EMPTY << path << EMPTY << HTTP_VERSION << WRAP;
+    // 写请求头
+    for (auto &item : this->header)
+    {
+        stream << item.first << ": " << joinValues(item.second) << WRAP;
+    }
+    stream << WRAP;
+    write(socketFd, stream.str().c_str(), stream.str().length());
+    // 写请求体
+    if (this->body && this->bodyLength > 0)
+    {
+        write(socketFd, this->body, this->bodyLength);
+    }
+
+    // 读取回复
+    OsStringStream responseStream;
+    char buffer[BUFFER_SIZE];
+    int bytesReceived;
+    size_t contentLength = 0;
+
+    // 读取响应头
+    String headers;
+    while ((bytesReceived = read(socketFd, buffer, 1)) > 0)
+    {
+        headers.append(buffer, 1);
+        if (buffer[0] == '\n')
+        {
+            // 当读取到两个连续的换行符时表示找到了响应头的结束
+            if (headers.substr(headers.length() - 4) == "\r\n\r\n")
+            {
+                break;
+            }
+        }
+    }
+
+    // 解析响应头
+    std::istringstream headersStream(headers);
+    std::string headerLine;
+    while (std::getline(headersStream, headerLine) && headerLine != "\r")
+    {
+        if (headerLine.find("Content-Length:") != std::string::npos)
+        {
+            contentLength = std::stoi(headerLine.substr(16));
+            break;
+        }
+    }
+    responseStream << headers;
+
+    // 读取响应体
+    if (contentLength > 0)
+    {
+        std::cout << "contentLength=" << contentLength << std::endl;
+        size_t remaining = contentLength;
+        while (remaining > 0 && (bytesReceived = read(socketFd, buffer, std::min(remaining, size_t(BUFFER_SIZE)))) > 0)
+        {
+            std::cout << "contentLength读取一次" << std::endl;
+            buffer[bytesReceived] = '\0';
+            responseStream << buffer;
+            remaining -= bytesReceived;
+            std::cout << "remaining=" << remaining << ",bytesReceived=" << bytesReceived << std::endl;
+        }
+        if (bytesReceived < 0)
+        {
+            perror("recv");
+            close(socketFd);
+            exit(1);
+        }
+    }
+
+    std::cout << "响应数据=" << responseStream.str() << std::endl;
+
+    // 关闭连接
+    close(socketFd);
 }
 
 String HttpClient::assembleUrl()
@@ -148,6 +259,14 @@ String HttpClient::encodeQueryParameters()
         }
     }
     return queryStream.str();
+}
+
+void HttpClient::set_socket_timeout(int fd, int seconds)
+{
+    struct timeval timeout;
+    timeout.tv_sec = seconds;
+    timeout.tv_usec = 0;
+    setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
 }
 
 int main()
--- httpclient/httpclient.h
+++ httpclient/httpclient.h
@@ -7,6 +7,7 @@
 #include <map>
 #include <vector>
 #include <regex>
+#include <unistd.h>
 
 using String = std::string;
 using OsStringStream = std::ostringstream;
@@ -17,6 +18,8 @@ using Header = std::map<String, Vector<String>>;
 constexpr const int BUFFER_SIZE = 4096;
 constexpr const char *HTTP_VERSION = "HTTP/1.1";
 constexpr const char *CONNETION_TYPE = "Connection: close\r\n";
+constexpr const char *WRAP = "\r\n";
+constexpr const char *EMPTY = " ";
 
 std::regex url_regex(R"((https?)://([^/\r\n]+)(/[^\r\n?]*)?(\?[^#\r\n]*)?)");
 
@@ -32,22 +35,29 @@ private:
     String method;
     Header header;
     Header query;
+    char *body;
+    size_t bodyLength;
 
 public:
     HttpClient(/* args */) = default;
     ~HttpClient() = default;
     HttpClient *setQuery(const String &key, const String &value);
     HttpClient *addQuery(const String &key, const String &value);
+    HttpClient *setBody(char *body, size_t bodyLength);
     HttpClient *post(const String &url);
     HttpClient *get(const String &url);
     HttpClient *doSend(const String &method, const String &url);
 
 private:
-    static String host_to_ip(String hostname);
+    static String host_to_ip(const String &hostname);
 
-    int http_create_socket(String ip);
+    static String joinValues(const Vector<String> &values);
 
-    Header parseQuery(const String& queryRaw);
+    static void set_socket_timeout(int fd, int seconds);
+
+    int http_create_socket(const String &ip);
+
+    Header parseQuery(const String &queryRaw);
 
     void send();
 
